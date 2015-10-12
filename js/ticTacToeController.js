b016e7bf878965c1a370d898393246cd269a2894
Paco Contreras <pacocontrerasdct@gmail.com>
2015-10-12 22:46:17 +0100

    Add some style and showing a single move on page

--- js/ticTacToeController.js
+++ js/ticTacToeController.js
@@ -2,7 +2,47 @@ angular.module('tictactoeApp')
   .controller('TicTacToeController', TicTacToeController);
 
 function TicTacToeController() {
+  this.allWiningPositions = [
+    "WWW------",
+    "---WWW---",
+    "------WWW",
+    "W--W--W--",
+    "-W--W--W-",
+    "--W--W--W",
+    "W---W---W",
+    "--W-W-W--"
+  ]
+  var wining = this.allWiningPositions;
 
-  
+  this.moves = [];
+
+  //move = this.exampleMove = "x---x---x";
+
+  //this.equals = angular.equals(this.allWiningPositions[1], this.exampleMove);
+  this.newMove = { move: '' };
+
+  this.add = addMove;
+
+  function addMove() {
+    este = this.moves.push(this.newMove.move);
+    esto = this.newMove.move;
+    console.log(este);
+    console.log(esto);
+  }
+
+  // this.check = checkForWinner(move, wining);
+
+  // function checkForWinner(move, wining) {    
+  //   for (var i = 0; i < wining.length; i++) {   
+  //     equals = angular.equals(wining[i], move);
+  //     if(equals === true) {
+  //       this.equals = equals;
+  //       break;
+  //     }
+  //     else {
+  //       this.equals = 'No match';
+  //     }
+  //   };
+  //}
 
 }
